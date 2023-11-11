a99577935dc80f05a941a8f3b65264fd0237b4a9
Fryskin <gman358325@gmail.com>
2023-11-11 09:35:36 +0100

    Blocked endpoints.

--- src/shopper/router.py
+++ src/shopper/router.py
@@ -21,41 +21,54 @@ router = APIRouter(
 @router.get("/retrieve")
 @cache(expire=600)
 async def get_all_products(limit: int = 10, offset: int = 0, session: AsyncSession = Depends(get_async_session),
-                           ):
+                           user: User = Depends(current_user)):
+    if user.active is False:
+        return {"code": 401, "message": "Unauthorized"}
+
     query = select(product)
     result = await session.execute(query)
+
     return result.mappings().all()[offset:][:limit]
 
 
 @router.post("/create")
 async def add_product(new_product: ProductCreate, session: AsyncSession = Depends(get_async_session),
-                      ):
+                      user: User = Depends(current_user)):
+    if user.active is False:
+        return {"code": 401, "message": "Unauthorized"}
 
     stmt = insert(product).values(**new_product.dict())
     await session.execute(stmt)
     await session.commit()
+
     return {"status": "The product was successfully created"}
 
 
 @router.put("/update")
 async def update_product(product_id: int, new_stmt: ProductUpdate, session: AsyncSession = Depends(get_async_session),
-                         ):
+                         user: User = Depends(current_user)):
+    if user.active is False:
+        return {"code": 401, "message": "Unauthorized"}
 
     stmt = update(product).values(**new_stmt.dict()).where(product.c.id == product_id)
     await session.execute(stmt)
     await session.commit()
+
     return {"status": "The product was successfully updated"}
 
 
 @router.delete("/delete")
+# async def delete_product(product_id: int, session: AsyncSession = Depends(get_async_session)):
 async def delete_product(product_id: int, session: AsyncSession = Depends(get_async_session),
-                         ):     #user: User = Depends(current_user)
+                         user: User = Depends(current_user)):
 
-    # if user.active is False:
-    #     return {"code": 401, "message": "Unauthorized"}
+    if user.active is False:
+        return {"code": 401, "message": "Unauthorized"}
+
     stmt = delete(product).where(product.c.id == product_id)
     await session.execute(stmt)
     await session.commit()
+
     return {"status": "The product was successfully deleted"}
 
 
